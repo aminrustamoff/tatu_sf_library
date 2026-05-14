92ac5709b31a45aa395142524e8ba9d79f92ef7c
Aminjon Rustamov <aminrustamov0424@gmail.com>
2026-05-14 11:36:21 +0500

    Fix URL issue

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     ".railway.app",
     ".up.railway.app",
+    "yusupov-feruz.up.railway.app",
 ]
 
 
@@ -167,7 +168,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STORAGES = {
-    "default": {
+    "default": {    
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
